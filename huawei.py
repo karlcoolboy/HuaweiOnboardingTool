b870f51b4a2ca5ffbebe710fe6d72527e61d1c48
karl <96855431+karlcoolboy@users.noreply.github.com>
2025-04-17 22:24:54 +0800

    Add files via upload

---
+++ huawei.py
@@ -0,0 +1,9 @@
+import webbrowser
+import urllib.parse
+
+
+# 构建mailto链接
+mailto_link = f"https://github.com/karlcoolboy/HuaweiOnboardingTool/blob/main/README.md#%E4%BD%BF%E7%94%A8%E6%96%B9%E5%BC%8F"
+
+# 打开默认邮件客户端
+webbrowser.open(mailto_link)
